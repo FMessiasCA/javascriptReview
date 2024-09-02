b769e9c730c6c8961d064fab129f48ba9f304251
Messias <fmessiasca@gmail.com>
2024-09-02 19:44:25 -0400

    Add Lesson 1 and half of lesson 2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-function parOuImpar () {
-    let par = 24;
-    let impar = 17;
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
 
-    ()
-}
+const isNew = document.getElementById('isNew');
+const isOld = document.getElementById('isOld');
+
+isNew.addEventListener('click', () => alert("Hellow World!"));
+isOld.addEventListener('click', () => alert("Hi, nice to see u again!!! :)"));
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+const sum = document.getElementById('sum');
+
+
+sum.addEventListener('click', () => {
+    const numberA = document.getElementById('numberA');
+    console.log(numberA);
+    console.log(num);
+    const numberB = document.getElementById('numberB');
+    
+})
+
+/* function sum(numberA, numberB) {
+    
+} */
