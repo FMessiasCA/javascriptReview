1ad1f47fd041bba41f216b42c1a961db3d80f061
Messias <fmessiasca@gmail.com>
2024-09-04 10:49:45 -0400

    Add Lesson 2: Sum of two numbers

--- scripts.js
+++ scripts.js
@@ -3,19 +3,23 @@
 const isNew = document.getElementById('isNew');
 const isOld = document.getElementById('isOld');
 
-isNew.addEventListener('click', () => alert("Hellow World!"));
+isNew.addEventListener('click', () => alert("Hello World!"));
 isOld.addEventListener('click', () => alert("Hi, nice to see u again!!! :)"));
 
 // Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
 const sum = document.getElementById('sum');
 
 
-sum.addEventListener('click', () => {
-    const numberA = document.getElementById('numberA');
-    console.log(numberA);
-    console.log(num);
-    const numberB = document.getElementById('numberB');
+sum.addEventListener('click', (event) => {
+    event.preventDefault();
+    const numberA = document.getElementById('numberA').value;
+    const numberB = document.getElementById('numberB').value;
+    const showResult = document.getElementById('showResult');
     
+    const sumTotal = Number(numberA) + Number(numberB);
+
+
+    return showResult.innerText = sumTotal < 9 ? '0' + sumTotal : sumTotal;
 })
 
 /* function sum(numberA, numberB) {
